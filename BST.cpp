fd0267d808e8b0bdd27587ab8d7c1092d9c193c7
gurpreet singh <amansingh9569@gmail.com>
2018-11-23 21:36:21 +0530

    bst

--- BST.cpp
+++ BST.cpp
@@ -1,33 +1,45 @@
 #include<iostream>
 #include<stdlib.h>
+
+class Node {
+	public:
+		Node *right;
+		Node * left;
+		int info;
+		Node(int info) {
+			this->info = info;
+			right = left = NULL;
+		}
+};
+
 class BST
 {
 	private:
-		struct node {
-			node *right;
-			node *left;
-			int info;
-		};
-		node **start;
-		node * insert(int info, node * root) {
+		Node *start;
+		Node * insert(int info, Node * root) {
 			if(root == NULL) {
-				node *newnode = newnode;
-				newnode->right = NULL;
-				newnode->left = NULL;
-				newnode->info = info;
-				return newnode;
+				Node *newnode = new Node(info);
+				root = newnode;
 			} else {
 				if(root->info < info) {
-					insert(info,root->right);
+					root->right = insert(info,root->right);
 				} else {
-					insert(info,root->left);
+					root->left = insert(info,root->left);
 				}
 			}
 			return root;
 		}
-		void traverse(int method, node *root) {
-			while(root) {
-				if(method == 0) {
+		void traverse(int method, Node *root) {
+			if(root!=NULL) {
+				if(method == -1) {
+					std::cout<<root->info<<" ";
+					traverse(method,root->left);
+					traverse(method,root->right);
+				} else if(method == 1) {
+					traverse(method,root->left);
+					traverse(method,root->right);
+					std::cout<<root->info<<" ";
+				} else {
 					traverse(method,root->left);
 					std::cout<<root->info<<" ";
 					traverse(method,root->right);
@@ -36,25 +48,42 @@ class BST
 		}
 	public:
 		BST() {
-			*start = NULL;
+			start = NULL;
 		}
 		void insertNode(int info) {
-			*start = this->insert(info,*start);
+			start = this->insert(info,start);
 		}
-		void traverseNodes(int method = 0) {
-			this->traverse(method, *start);
+		void test() {
+			if(start) {
+				std::cout<<start->info;	
+				std::cout<<start->left->info;	
+				std::cout<<start->right->info;
+				std::cout<<start->right->left;
+			}
+		}
+		void traverseNodes(int method = 0, bool print = true) {
+			if(method == -1 && print) {
+				std::cout<<"preOrder = ";
+			} else if(method == 1 && print) {
+				std::cout<<"postOrder = ";
+			} else if(method == 0 && print){
+				std::cout<<"inOrder = ";
+			}
+			traverse(method, start);
+			std::cout<<std::endl;
 		}
 };
-
-
 main()
 {
 	BST *bst = new BST();
 	bst->insertNode(5);
+	bst->insertNode(0);
+	bst->insertNode(45);
 	bst->insertNode(4);
 	bst->insertNode(8);
 	bst->insertNode(10);
-	bst->insertNode(0);
 	bst->insertNode(9);
-	bst->traverseNodes();
+	bst->traverseNodes(0);
+	bst->traverseNodes(-1);
+	bst->traverseNodes(1);
 }
