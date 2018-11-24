d331ac25227b68293a37812e6b4c5ea8e1bd12d1
gurpreet singh <amansingh9569@gmail.com>
2018-11-24 19:30:18 +0530

    bst questions

--- BST.cpp
+++ BST.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
+#include<stack>
+#include <queue> 
 
 class Node {
 	public:
@@ -9,6 +11,10 @@ class Node {
 		Node(int info) {
 			this->info = info;
 			right = left = NULL;
+		}
+		~Node() {
+			right = NULL;
+			left = NULL;
 		}
 };
 
@@ -46,6 +52,145 @@ class BST
 				}
 			}
 		}
+		int height(Node *root) {
+			if (root == NULL) {
+				return 0;
+			} else {
+				int r = height(root->left);
+				int l = height(root->right);
+				int max = r>l?r:l;
+				return max+1;
+			}
+		}
+		int maxValue(Node *root) {
+	        if(root == NULL) {
+	            return -1;
+	        } else {
+	        	int res = root->info;
+				int r = maxValue(root->right);
+	            int l = maxValue(root->left);
+	            res = res > r ? res : r;
+	            res = res > l ? res : l;
+	            return res;
+	        }
+    	}
+    	int minValue(Node *root) {
+		        if(root == NULL) {
+		            return 10000;
+		        } else {
+		        	int res = root->info;
+					int r = minValue(root->right);
+		            int l = minValue(root->left);
+		            res = res < r ? res : r;
+		            res = res < l ? res : l;
+		            return res;
+		        }
+		}
+		Node* maxNode(Node *root) {
+	        if(root == NULL) {
+	            return NULL;
+	        } else {
+	        	Node* res = root;
+				Node* r = maxNode(root->right);
+	            Node* l = maxNode(root->left);
+	            if(r)
+	            	res = res->info > r->info ? res : r;
+	            if(l)
+	            	res = res->info > l->info ? res : l;
+	            return res;
+	        }
+    	}
+    	Node* minNode(Node *root) {
+	        if(root == NULL) {
+	            return NULL;
+	        } else {
+	        	Node* res = root;
+				Node* r = minNode(root->right);
+	            Node* l = minNode(root->left);
+	            if(r)
+	            	res = res->info < r->info ? res : r;
+	            if(l)
+	            	res = res->info < l->info ? res : l;
+	            return res;
+	        }
+    	}
+		Node * find(Node *root, int val) {
+			if(root == NULL) {
+				return NULL;
+			} else {
+				if(root->info == val) {
+					return root;
+				}
+				if(root->info > val) {
+					return find(root->left,val);
+				} else {
+					return find(root->right,val);
+				}
+			}
+		}
+		Node * _findParent(Node *root, int val) {
+			if(root == NULL) {
+				return NULL;
+			}
+			if(root->info == val)
+				return root;
+			if(root->left == NULL && root->right == NULL) {
+				return NULL;
+			}
+			if (root->left || root->right)
+				if(root->left)
+					if(root->left->info == val)
+						return root;
+				if(root->right)
+					if(root->right->info == val)
+						return root;
+			if(root->info > val && root->left) {
+				return _findParent(root->left,val);
+			} else {
+				return _findParent(root->right,val);
+			}
+		}
+		Node * _deleteNode(Node *root,int val) {
+			if(root == NULL)
+				return root;
+			if(val < root->info)
+				root->left = _deleteNode(root->left,val);
+			else if(val > root->info)
+				root->right = _deleteNode(root->right,val);
+			else {
+				if(!root->left) {
+					Node *temp = root->right;
+					delete root;
+					return temp;
+				}
+				if(!root->right) {
+					Node *temp = root->left;
+					delete root;
+					return temp;
+				}
+				Node * temp = maxNode(root->left);
+				root->info = temp->info;
+				root->left = _deleteNode(root->left,temp->info);
+				return root;
+			}
+		}
+		int _countNodes(Node *root) {
+			if(root == NULL) {
+				return 0;
+			}
+			if(root->left == NULL && root->right == NULL) {
+				return 1;
+			}
+			int r;
+			int l;
+			r = _countNodes(root->right);
+			l = _countNodes(root->left);
+			return r+l+1;
+		}
+		bool _checkExist(Node *root,int val) {
+			Node *node = find(root,val);
+			return node?true:false;
+		}
 	public:
 		BST() {
 			start = NULL;
@@ -61,6 +206,12 @@ class BST
 				std::cout<<start->right->left;
 			}
 		}
+		int getMaxValue() {
+			return maxNode(this->start)->info;
+		}
+		int getMinValue() {
+			return minNode(this->start)->info;
+		}
 		void traverseNodes(int method = 0, bool print = true) {
 			if(method == -1 && print) {
 				std::cout<<"preOrder = ";
@@ -72,18 +223,51 @@ class BST
 			traverse(method, start);
 			std::cout<<std::endl;
 		}
+		int findPar(int val) {
+			Node *temp = _findParent(start,val);
+			return temp?temp->info:-1;
+		}
+		int findHeight() {
+			return height(this->start);
+		}
+		void deleteNode(int val) {
+			start = _deleteNode(start,val);
+		}
+		bool checkExist(int val) {
+			return _checkExist(start,val);	
+		}
+		int count() {
+			return _countNodes(start);
+		}
 };
 main()
 {
 	BST *bst = new BST();
 	bst->insertNode(5);
-	bst->insertNode(0);
+	bst->insertNode(48);
 	bst->insertNode(45);
 	bst->insertNode(4);
 	bst->insertNode(8);
 	bst->insertNode(10);
 	bst->insertNode(9);
+	bst->insertNode(2);
+	bst->insertNode(12);
+	bst->insertNode(72);
+	bst->insertNode(95);
+	bst->insertNode(46);
+	bst->insertNode(47);
 	bst->traverseNodes(0);
 	bst->traverseNodes(-1);
 	bst->traverseNodes(1);
+	std::cout<<bst->findHeight()<<std::endl<<bst->getMaxValue()<<" "<<bst->getMinValue()<<" "<<bst->count()<<std::endl;
+	std::cout<<bst->findPar(12)<<" "<<bst->checkExist(100)<<std::endl;
+	bst->deleteNode(72);
+	bst->deleteNode(8);
+	bst->deleteNode(5);
+	bst->deleteNode(9);
+	bst->traverseNodes(0);
+	bst->traverseNodes(-1);
+	bst->traverseNodes(1);
+	std::cout<<bst->count();
+	
 }
---
+++ checkBST.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<stdlib.h>
+#include<queue>
+class Node {
+	public:
+		Node *right;
+		Node * left;
+		int data;
+		Node(int info) {
+			this->data = info;
+			right = left = NULL;
+		}
+};
+int maxValue(Node *root) {
+        if(root == NULL) {
+            return -1;
+        } else {
+        	int res = root->data;
+			int r = maxValue(root->right);
+            int l = maxValue(root->left);
+            res = res > r ? res : r;
+            res = res > l ? res : l;
+            return res;
+        }
+}
+
+int minValue(Node *root) {
+        if(root == NULL) {
+            return 100000;
+        } else {
+        	int res = root->data;
+			int r = minValue(root->right);
+            int l = minValue(root->left);
+            res = res < r ? res : r;
+            res = res < l ? res : l;
+            return res;
+        }
+}
+
+bool checkBST(Node* root) {
+        
+        if(root->left != NULL) {
+            if(maxValue(root->left)> root->data)
+                return false;
+        } 
+        if(root->right != NULL) {
+            if(minValue(root->right) < root->data)
+                return false;
+        }
+        bool r;
+        bool l;
+        l = root->left!=NULL?checkBST(root->left):true;
+        r = root->right!=NULL?checkBST(root->right):true;
+        if(r == false || l == false)
+            return false;
+        return true;
+	}
+
+bool isSimilar(Node *r1,Node *r2) {
+	if(r1 == NULL && r2 != NULL) {
+		return false;
+	}
+	if( r1 != NULL && r2 == NULL) {
+		return false;
+	}
+	if( r1 == NULL && r2 == NULL) {
+		return true;
+	}
+	if(!r1->right && !r1->left && !r2->left && !r2->right ) {
+		return true;
+	}
+	bool r = isSimilar(r1->right,r2->right);
+	bool l = isSimilar(r1->left,r2->left);
+	if (r == true && l == true)
+		return true;
+	return false;
+}
+
+bool isEqual(Node *r1,Node *r2) {
+	if(r1 == NULL && r2 != NULL) {
+		return false;
+	}
+	if( r1 != NULL && r2 == NULL) {
+		return false;
+	}
+	if( r1 == NULL && r2 == NULL) {
+		return true;
+	}
+	if((r1->data == r2->data) && !r1->right && !r1->left && !r2->left && !r2->right ) {
+		return true;
+	}
+	if((r1->data != r2->data) && !r1->right && !r1->left && !r2->left && !r2->right ) {
+		return false;
+	}
+	bool r = isEqual(r1->right,r2->right);
+	bool l = isEqual(r1->left,r2->left);
+	if (r == true && l == true && r1->data == r2->data )
+		return true;
+	return false;
+}
+ bool isMirrorImage(Node *r1,Node *r2) {
+ 	if(r1 == NULL && r2 == NULL) {
+ 		return true;
+	}
+	if(!r1 || !r2) {
+		return false;
+	}
+	if( r1->data == r2->data ) {
+		bool r = isMirrorImage (r1->right,r2->left);
+		bool l = isMirrorImage (r1->left,r2->right);
+		return r && l;
+	}
+	return false;
+ }
+
+main() {
+	Node * r1,*r2,*r3;
+	r1 = new Node(4);
+	r1->left = new Node(5);
+	r1->right = new Node(7);
+	r1->left->left = new Node(8);
+	r1->left->right = new Node(4);
+	r1->right->left = new Node(6);
+	r1->right->right = new Node(10);
+	r1->right->right->right = new Node(10);
+	r2 = new Node(4);
+	r2->left = new Node(5);
+	r2->right = new Node(7);
+	r2->left->left = new Node(8);
+	r2->left->right = new Node(4);
+	r2->right->left = new Node(6);
+	r2->right->right = new Node(0);
+	r2->right->right->right = new Node(10);
+	std::cout<<isSimilar(r1,r2)<<std::endl;
+	std::cout<<isEqual(r1,r2)<<std::endl;
+	r3 = new Node(4);
+	r3->right = new Node(45);
+	r3->left = new Node(45);
+	std::cout<<isMirrorImage(r3,r3)<<std::endl;
+}
