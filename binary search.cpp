6b15bf6640cb3fc09533d3e019e612efe46dafed
gurpreet singh <amansingh9569@gmail.com>
2018-12-05 16:38:41 +0530

    bsi

--- binary search.cpp
+++ binary search.cpp
@@ -12,9 +12,23 @@ int bs(int a[],int search, int start, int end) {
 		return bs(a,search,mid+1,end);
 }
 
+int bsi(int a[],int search, int start, int end) {
+	int mid = (start + end)/2;
+	while(a[mid] != search) {
+		pass+=1;
+		if(a[mid] > search) {
+			end = mid - 1;
+		} else {
+			start = mid + 1;
+		}
+		mid = (start + end)/2;
+	}
+	return mid;
+}
+
 main() {
 	int a[] = {1,2,3,4,5,6,8,9,45,56};
-	std::cout<<bs(a,1,0,10)<<std::endl;
+	std::cout<<bsi(a,3,0,10)<<std::endl;
 	std::cout<<pass;	 
 }
 
